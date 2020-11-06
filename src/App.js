a0bb491cbfa0f5302cd15d2745d16f643a36c2f4
ledney07 <ledney21@gmail.com>
2020-11-06 02:44:24 -0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>reviews</h1>
+    </div>
+  );
+}
+
+export default App;
+
+//This application example of useEffect and Conditional Rendering
